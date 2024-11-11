2ce985aedc797a831dcd5f07dabd0d646c3b0e84
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-11-11 12:11:10 +0100

    a

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict'
-const rowName='ABCDEFGH';
+const rowName='ABCDEFGH'
 const initialPosition = 'D4'
 
 window.addEventListener('load', () => {
@@ -8,77 +8,87 @@ window.addEventListener('load', () => {
 
 
 function renderTable() {
-	let tabla='';
+	let tabla=''
 	// Dibujamos la tabla de 8x8 más la primera celda de cada fila que es el nombre de la fila de 8..1
 	for (let i=8; i>0; i--) {
-		tabla+=`<tr><th>${i}</th>`;
+		tabla+=`<tr><th>${i}</th>`
 		for (let j=0; j<8; j++) {
 			tabla+=`
-				<td id="${rowName.charAt(j)+i}" class="${((i+j)%2)?'negra':'blanca'}"></td>`;
+				<td id="${rowName.charAt(j)+i}" class="${((i+j)%2)?'negra':'blanca'}"></td>`
 		}
-		tabla+='</tr><tr><th></th>';
+		tabla+='</tr><tr><th></th>'
 	}
 	// Dibujamos el encabezado de las columnas, A..H
 	for (let i=0;i<8;i++) {
-		tabla+=`<th>${rowName.charAt(i)}</th>`;
+		tabla+=`<th>${rowName.charAt(i)}</th>`
 	}
-	tabla+='</tr>';
+	tabla+='</tr>'
 
-	tablero.innerHTML=tabla;
+	tablero.innerHTML=tabla
 
 	// Ahora podríamos colocar el caballo en su celda. 
 	// Para que quepa dadle un ancho de 50px
 	document.getElementById(initialPosition).innerHTML = 
-		'<img id="caballo" src="./images/caballo.png" width="50px">';
+		'<img id="caballo" src="./images/caballo.png" width="50px">'
 }
 
 function calcTargets(position) {
-	const column = rowName.indexOf(position.charAt(0)) + 1;
-	const row = Number(position.charAt(1));
-	let targets = [];
-	targets.push([column+2, row+1]);
-	targets.push([column+2, row-1]);
-	targets.push([column-2, row+1]);
-	targets.push([column-2, row-1]);
-	targets.push([column+1, row+2]);
-	targets.push([column+1, row-2]);
-	targets.push([column-1, row+2]);
-	targets.push([column-1, row-2]);
+	const column = rowName.indexOf(position.charAt(0)) + 1
+	const row = Number(position.charAt(1))
+	let targets = []
+	targets.push([column+2, row+1])
+	targets.push([column+2, row-1])
+	targets.push([column-2, row+1])
+	targets.push([column-2, row-1])
+	targets.push([column+1, row+2])
+	targets.push([column+1, row-2])
+	targets.push([column-1, row+2])
+	targets.push([column-1, row-2])
 	return clearTargets(targets).map(cell => 
-		rowName.charAt(cell[0]-1) + cell[1]);
+		rowName.charAt(cell[0]-1) + cell[1])
 }
 
 function clearTargets(targets) {
 	return targets.filter(cell => {
-		const column = cell[0];
-		const row = cell[1];
+		const column = cell[0]
+		const row = cell[1]
 		return (column >=1 && column <=8
 			&& row >= 1 && row <= 8)
 	})
 }
 
 function getRandomPosition() {
-	const columnas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-	const filas = ['1', '2', '3', '4', '5', '6', '7', '8'];
-	const columnaAleatoria = columnas[parseInt(Math.random() * columnas.length)];
-	const filaAleatoria = filas[parseInt(Math.random() * filas.length)];
-	return columnaAleatoria + filaAleatoria;
+	const columnas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+	const filas = ['1', '2', '3', '4', '5', '6', '7', '8']
+	const columnaAleatoria = columnas[parseInt(Math.random() * columnas.length)]
+	const filaAleatoria = filas[parseInt(Math.random() * filas.length)]
+	return columnaAleatoria + filaAleatoria
 }
 
 function restartScore() {
 	document.querySelector('.btn-reinicio').addEventListener('click', (event) => {
 		alert(event.target)
-		document.getElementById('played-text').innerHTML = '0';
-		document.getElementById('best-mov-text').innerHTML = '0';
-		document.getElementById('best-num-text').innerHTML = '0';
-		document.getElementById('worst-mov-text').innerHTML = '0';
-		document.getElementById('worst-num-text').innerHTML = '0';
+		document.getElementById('played-text').innerHTML = '0'
+		document.getElementById('best-mov-text').innerHTML = '0'
+		document.getElementById('best-num-text').innerHTML = '0'
+		document.getElementById('worst-mov-text').innerHTML = '0'
+		document.getElementById('worst-num-text').innerHTML = '0'
 	})
 }
 
 function putDropable() {
-	const celdasDropables = calcTargets(initialPosition);
+	const celdasDropables = calcTargets(initialPosition)
 	celdasDropables.forEach(celda => {
-		document.getElementById(celda).classList.add('dropable');
+		document.getElementById(celda).classList.add('dropable')
 	})
+}
+
+function winnerPosition() {
+	const columnas = ['A', 'B', 'C', 'E', 'F', 'G', 'H']
+	const filas = ['1', '2', '3', '4', '5', '6', '7', '8']
+	const columnaAleatoria = columnas[parseInt(Math.random() * columnas.length)]
+	const filaAleatoria = filas[parseInt(Math.random() * filas.length)]
+	const casillaResultante = columnaAleatoria + filaAleatoria
+	const casilla = document.getElementById(casillaResultante).classList.add('final')
+	return casilla
 }
